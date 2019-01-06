fb72aa7000cb592267fcb9b72bee83053fbb9a76
Tim Fielder <fiel1139@gmail.com>
2019-01-06 12:47:19 -0700

    Creates initial template for api/v1/results?location endpoint spec

---
+++ spec/requests/api/v1/results_request_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe 'api/v1/results' do
+  it 'returns a successful response' do
+
+    VCR.use_cassette('denver_results_request') do
+      get '/api/v1/results?location=denver,co'
+    end
+
+    expect(response).to be_successful
+    # result = JSON.parse(response.body, :symbolize_names => true)
+    #
+    # expect(result[:weather][:latitude]).to eq(39.7392358)
+    # expect(result[:weather][:longitude]).to eq(-104.990251)
+    # expect(response).to be_successful
+  end
+end
