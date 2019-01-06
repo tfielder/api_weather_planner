95c540b435718eb0714240a6ce39786fc9edeb46
Tim Fielder <fiel1139@gmail.com>
2019-01-06 10:33:10 -0700

    Adds testing to weather request spec for verification of results.

--- spec/requests/api/v1/weather_request_spec.rb
+++ spec/requests/api/v1/weather_request_spec.rb
@@ -2,13 +2,15 @@ require 'rails_helper'
 
 describe 'api/v1/weather' do
   it 'returns a successful response' do
-    # sample_response = {}
 
     VCR.use_cassette('denver_weather_request') do
       get '/api/v1/weather?location=denver,co'
     end
 
     expect(response).to be_successful
-    #expect(JSON.parse(response.body, :symbolize_names => true)).to eq(sample_response)
+    result = JSON.parse(response.body, :symbolize_names => true)
+    expect(result[:latitude]).to eq(39.7392358)
+    expect(result[:longitude]).to eq(-104.990251)
+    expect(response).to be_successful
   end
 end
