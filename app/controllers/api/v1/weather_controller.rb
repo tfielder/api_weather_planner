d794161a00bb46c6a7d29a14d2aa9fb7c4addf00
Tim Fielder <fiel1139@gmail.com>
2019-01-06 10:39:42 -0700

    Modifies weather endpoint and weather request service spec to include location and weather.

--- app/controllers/api/v1/weather_controller.rb
+++ app/controllers/api/v1/weather_controller.rb
@@ -3,6 +3,10 @@ class Api::V1::WeatherController < ApplicationController
     location = GoogleGeoRequest.new(params[:location]).info
     #location = {:coordinates=>{"lat"=>39.7392358, "lng"=>-104.990251}, :address=>"Denver, CO, USA"}
     result = DarkSkyRequest.new(location[:coordinates]).weather
-    render json: result
+    weather = {
+      "location": location,
+      "weather": result
+    }
+    render json: weather
   end
 end
--- spec/requests/api/v1/weather_request_spec.rb
+++ spec/requests/api/v1/weather_request_spec.rb
@@ -9,8 +9,9 @@ describe 'api/v1/weather' do
 
     expect(response).to be_successful
     result = JSON.parse(response.body, :symbolize_names => true)
-    expect(result[:latitude]).to eq(39.7392358)
-    expect(result[:longitude]).to eq(-104.990251)
+
+    expect(result[:weather][:latitude]).to eq(39.7392358)
+    expect(result[:weather][:longitude]).to eq(-104.990251)
     expect(response).to be_successful
   end
 end
