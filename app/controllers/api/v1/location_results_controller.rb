92f0658a2f8d852a087b7154e4809bd6d2a59e9a
Tim Fielder <fiel1139@gmail.com>
2019-01-06 12:52:14 -0700

    Creates location results controller.

---
+++ app/controllers/api/v1/location_results_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::LocationResultsController < ApplicationController
+  def index
+    location = GoogleGeoRequest.new(params[:location]).info
+    weather = DarkSkyRequest.new(location[:coordinates]).weather
+    events = GoogleEventsRequest.new(params[:location]).events
+    results = {
+      "location": location,
+      "weather": weather,
+      "events": events
+    }
+    render json: results
+  end
+end
