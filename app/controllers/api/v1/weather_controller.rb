13bcba4dafa996740e72ecd20f2b6626ea6243bb
Tim Fielder <fiel1139@gmail.com>
2019-01-06 10:40:14 -0700

    Remove note in weather controller.

--- app/controllers/api/v1/weather_controller.rb
+++ app/controllers/api/v1/weather_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::WeatherController < ApplicationController
   def index
     location = GoogleGeoRequest.new(params[:location]).info
-    #location = {:coordinates=>{"lat"=>39.7392358, "lng"=>-104.990251}, :address=>"Denver, CO, USA"}
     result = DarkSkyRequest.new(location[:coordinates]).weather
     weather = {
       "location": location,
