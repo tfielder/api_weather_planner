fcda5e5460d9376a695481b8decc11f80ff6e608
Tim Fielder <fiel1139@gmail.com>
2019-01-06 17:50:59 -0700

    Comments out request for events.

--- app/controllers/api/v1/location_results_controller.rb
+++ app/controllers/api/v1/location_results_controller.rb
@@ -2,7 +2,8 @@ class Api::V1::LocationResultsController < ApplicationController
   def index
     location = GoogleGeoRequest.new(params[:location]).info
     weather = DarkSkyRequest.new(location[:coordinates]).weather
-    events = GoogleEventsRequest.new(params[:location]).events
+    #events = GoogleEventsRequest.new(params[:location]).events
+    events = "NA"
     results = {
       "location": location,
       "weather": weather,
