12c10f7ed4089f875532c94221e67af9108349fc
Tim Fielder <fiel1139@gmail.com>
2019-01-06 17:59:04 -0700

    Revises location_results endpoint and removes others temporarily.

--- app/controllers/api/v1/location_results_controller.rb
+++ app/controllers/api/v1/location_results_controller.rb
@@ -2,12 +2,9 @@ class Api::V1::LocationResultsController < ApplicationController
   def index
     location = GoogleGeoRequest.new(params[:location]).info
     weather = DarkSkyRequest.new(location[:coordinates]).weather
-    #events = GoogleEventsRequest.new(params[:location]).events
-    events = "NA"
     results = {
       "location": location,
-      "weather": weather,
-      "events": events
+      "weather": weather
     }
     render json: results
   end
--- spec/requests/api/v1/location_results_request_spec.rb
+++ spec/requests/api/v1/location_results_request_spec.rb
@@ -8,10 +8,9 @@ describe 'api/v1/location_results' do
     end
 
     expect(response).to be_successful
-    # result = JSON.parse(response.body, :symbolize_names => true)
-    #
-    # expect(result[:weather][:latitude]).to eq(39.7392358)
-    # expect(result[:weather][:longitude]).to eq(-104.990251)
-    # expect(response).to be_successful
+    result = JSON.parse(response.body, :symbolize_names => true)
+    expect(result[:weather][:latitude]).to eq(39.7392358)
+    expect(result[:weather][:longitude]).to eq(-104.990251)
+    expect(response).to be_successful
   end
 end
