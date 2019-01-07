d95b022d8599d645bbf20e70cb72c11fc7cf0dbe
Tim Fielder <fiel1139@gmail.com>
2019-01-06 17:50:42 -0700

    Removes pry from google events request.

--- app/services/google_events_request.rb
+++ app/services/google_events_request.rb
@@ -2,7 +2,6 @@ class GoogleEventsRequest
   def initialize(location)
     @loction = location
     @google_events_response = get_google_events_response
-    binding.pry
   end
 
   def events
@@ -18,7 +17,6 @@ class GoogleEventsRequest
   end
 
   def get_google_events_response
-    binding.pry
     @google_events_response = connection("https://www.googleapis.com/customsearch/v1?key=#{ENV['google_search_api_key']}&cx=#{ENV['cx']}&q=#{@location}")
   end
 
