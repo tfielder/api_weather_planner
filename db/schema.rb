a787589a747c828a2c1376bdec238e67be93cf61
Tim Fielder <fiel1139@gmail.com>
2019-01-07 17:22:02 -0700

    Creates schema for db.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_01_05_225550) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "dbs", force: :cascade do |t|
+  end
 
 end
